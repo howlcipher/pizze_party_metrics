cb53cf4704310944007ec21e1397c5eb7a1fcc20
howlcipher <wylelias.123@gmail.com>
2026-07-22 09:19:41 -0400

    chore: update repository name to pizza_party_metrics

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pizze_party_metrics/',
+  base: '/pizza_party_metrics/',
   plugins: [
     react(),
     tailwindcss(),
